e240e5e937e364244df1e95528493442bfac12ce
Karol Hulka <karolhulka@op.pl>
2024-01-20 15:13:59 +0100

    Revert "Main menu and loading screen (basic)"
    This reverts commit 3121b53f3ec99cf614ad8bab701c78c8d8cf64e2.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,7 +23,7 @@ public class GameManager : MonoBehaviour
     private float waitingToStartTimer = 1f;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 120f;
+    private float gamePlayingTimerMax = 10f;
 
     private void Awake()
     {
